81c125c1da783e55bdd10951e09d22da6effe0de
Justin Jones <justinj@portland.nwframing.com>
2016-12-19 09:23:41 -0800

    checkpoint

--- gulpfile.js
+++ gulpfile.js
@@ -9,16 +9,24 @@ var cleanCSS = require("gulp-clean-css");
 var rename = require('gulp-rename');
 var util = require("gulp-util");
 
-gulp.task('sass:default', function(){
-gulp.src('./src/sass/themes/default.scss')
-.pipe(sourcemaps.init())
-.pipe(sass())
-.pipe(rename('styles.css'))
-.pipe(sourcemaps.write())
-.pipe(gulp.dest('./src'));
+// gulp.task('sass:default', function(){
+// gulp.src('./src/sass/themes/default.scss')
+// .pipe(sourcemaps.init())
+// .pipe(sass())
+// .pipe(rename('test.css'))
+// .pipe(sourcemaps.write())
+// .pipe(gulp.dest('./src'));
+// });
+
+
+gulp.task('sass', function () {
+  gulp.src('./src/sass/themes/**.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
 });
+ 
 
 
-gulp.task('default', ['sass:default'], function() {
+gulp.task('default', ['sass'], function() {
     gulp.watch('*.scss', ['sass']);
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import {SITE } from './app.config';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'Test Test';
